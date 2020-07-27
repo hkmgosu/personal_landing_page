7085d98aef48c54950571e762a45ad9b1fdb9cc8
hkmgosu <erick.quiroz@live.com>
2020-07-27 04:41:47 -0400

    beta

---
+++ config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Erick Identity', // <title>
+  manifestName: 'Identity',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-identity/`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Erick Quiroz',
+  heading: 'Software Engineer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-linkedin',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/erick-quiroz-godoy',
+    },
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/hkmgosu',
+    },
+    {
+      icon: 'fa-instagram',
+      name: 'Facebook',
+      url: 'https://instagram.com/xxkukulkan',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:erick.quiroz@live.com',
+    }
+  ],
+};
