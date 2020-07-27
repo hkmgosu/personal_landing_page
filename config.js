0474f799c29d882fc3fe5fd8ba14ee794f8e69ff
hkmgosu <erick.quiroz@live.com>
2020-07-27 15:22:31 -0400

    config

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Erick Identity', // <title>
-  manifestName: 'Identity',
-  manifestShortName: 'Landing', // max 12 characters
+  siteTitle: 'Erick Quiroz, Software Engineer', // <title>
+  manifestName: 'Erick Quiroz Software Engineer',
+  manifestShortName: 'Erick Quiroz Landing', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-identity/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'Erick Quiroz',
   heading: 'Software Engineer',
   // social
